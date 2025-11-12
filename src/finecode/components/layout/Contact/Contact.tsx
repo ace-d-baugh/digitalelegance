d670f362078f2325d73ddbe5fafa8b03b3a669df
Ace Baugh <ace.d.baugh@gmail.com>
2025-11-11 20:46:57 -0500

    Contact form initialized

---
+++ src/finecode/components/layout/Contact/Contact.tsx
@@ -0,0 +1,18 @@
+// components/Contact/Contact.tsx
+
+import Button from '../../common/Button/Button';
+import './Contact.css';
+
+function Contact() {
+	return (
+		<div className="Contact">
+			<h2>CONTACT</h2>
+			
+			<div className="Contact-btn">
+				<Button text="Send" />
+			</div>
+		</div>
+	);
+}
+
+export default Contact;
